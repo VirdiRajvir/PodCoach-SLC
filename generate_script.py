64663033a6ae707b84b387b4f67e708002d5a2cd
VirdiRajvir <rajvirsinghvirdi1@gmail.com>
2025-06-10 16:50:33 +0800

    Script  summary is now saved to a file

--- generate_script.py
+++ generate_script.py
@@ -1,13 +1,15 @@
 from langchain_ollama import ChatOllama
 
-def generate_script(goal):
-    system_prompt = f"You are an AI coach helping users achieve {goal}. Generate a motivational coaching script that lasts about 20 minutes, encouraging and guiding the user through their goal. The script should be engaging, supportive, and provide actionable steps. Use a friendly and encouraging tone."
+def generate_script(goal, name):
+    system_prompt = f"You are an AI coach helping users achieve {goal} for {name}. Generate a motivational coaching script for {name} that lasts about 20 minutes, encouraging and guiding {name} through their goal. The script should be engaging, supportive, and provide actionable steps. Use a friendly and encouraging tone."
     llm = ChatOllama(model="llama3",
                  temperature=0.1)
 
     response = llm.invoke(system_prompt)
-    #save script to file
-    with open('static/script/coaching_script.txt', 'w') as file:
-        file.write(response.content)
 
+
+    system_prompt = f"You are a summarizer assistant. Summarize the following coaching transcript highlighting the key points and actionable steps: {response.content}"
+    with open('static/summary/summary.txt', 'w') as file:
+        summary_response = llm.invoke(system_prompt)
+        file.write(summary_response.content)
     return response.content
